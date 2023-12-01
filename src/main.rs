5004f57981fc96d75333af39c3b0c38745231218
Stefan Nikolaev Kochev <SKO@milestone.dk>
2023-12-01 15:59:37 +0200

    day 1

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Merry Christmas!"
 }
 
-// `StatusCode` gives an empty response with that status code
 async fn error_status() -> StatusCode {
     StatusCode::INTERNAL_SERVER_ERROR
+}
+
+async fn cube_the_bits(Path(param): Path<String>) -> impl IntoResponse {
+    param
+        .split('/')
+        .map(|x| x.parse::<i32>().unwrap())
+        .reduce(|acc, e| acc ^ e)
+        .unwrap()
+        .pow(3)
+        .to_string()
+        .into_response()
 }
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
-        .route("/-1/error", get(error_status));
+        .route("/-1/error", get(error_status))
+        .route("/1/*param", get(cube_the_bits));
 
     Ok(router.into())
 }
