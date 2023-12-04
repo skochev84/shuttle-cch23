60931d093abbedc8f0faf27bb94404f9d42598d4
Stefan Nikolaev Kochev <SKO@milestone.dk>
2023-12-04 15:05:18 +0200

    day4

---
+++ src/days/day4.rs
@@ -0,0 +1,157 @@
+use axum::{extract, http::StatusCode, Json};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+pub struct DeerStrength {
+    name: String,
+    strength: i32,
+}
+
+#[derive(Deserialize)]
+pub struct DeerContestant {
+    name: String,
+    strength: i32,
+    speed: f64,
+    height: i32,
+    antler_width: i32,
+    snow_magic_power: i32,
+    favorite_food: String,
+    #[serde(rename = "cAnD13s_3ATeN-yesT3rdAy")]
+    candies: i32,
+}
+
+#[derive(Serialize)]
+pub struct DeerWinners {
+    fastest: String,
+    tallest: String,
+    magician: String,
+    consumer: String,
+}
+
+pub async fn deer_strength(
+    extract::Json(payload): extract::Json<Vec<DeerStrength>>,
+) -> (StatusCode, String) {
+    let result = payload
+        .into_iter()
+        .reduce(|a, b| DeerStrength {
+            name: "Sum".to_string(),
+            strength: (a.strength + b.strength),
+        })
+        .unwrap()
+        .strength
+        .to_string();
+    (StatusCode::OK, result)
+}
+
+pub async fn candy(
+    extract::Json(payload): extract::Json<Vec<DeerContestant>>,
+) -> (StatusCode, Json<DeerWinners>) {
+    let fastest = payload
+        .iter()
+        .max_by(|a, b| a.speed.total_cmp(&b.speed))
+        .unwrap();
+
+    let tallest = payload
+        .iter()
+        .max_by(|a, b| a.height.cmp(&b.height))
+        .unwrap();
+
+    let magician = payload
+        .iter()
+        .max_by(|a, b| a.snow_magic_power.cmp(&b.snow_magic_power))
+        .unwrap();
+
+    let consumer = payload
+        .iter()
+        .max_by(|a, b| a.candies.cmp(&b.candies))
+        .unwrap();
+
+    let result = DeerWinners {
+        fastest: format!(
+            "Speeding past the finish line with a strength of {} is {}",
+            fastest.strength, fastest.name
+        ),
+        tallest: format!(
+            "{} is standing tall with his {} cm wide antlers",
+            tallest.name, tallest.antler_width
+        ),
+        magician: format!(
+            "{} could blast you away with a snow magic power of {}",
+            magician.name, magician.snow_magic_power
+        ),
+        consumer: format!(
+            "{} ate lots of candies, but also some {}",
+            consumer.name, consumer.favorite_food
+        ),
+    };
+    (StatusCode::OK, axum::Json(result))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_candies() {
+        let deers = vec![
+            DeerContestant {
+                name: "Dasher".to_string(),
+                strength: 5,
+                speed: 50.4,
+                height: 80,
+                antler_width: 36,
+                snow_magic_power: 9001,
+                favorite_food: "hay".to_string(),
+                candies: 2,
+            },
+            DeerContestant {
+                name: "Dancer".to_string(),
+                strength: 6,
+                speed: 48.2,
+                height: 65,
+                antler_width: 37,
+                snow_magic_power: 4004,
+                favorite_food: "grass".to_string(),
+                candies: 5,
+            },
+        ];
+        let res = DeerWinners {
+            fastest: "Speeding past the finish line with a strength of 5 is Dasher".to_string(),
+            tallest: "Dasher is standing tall with his 36 cm wide antlers".to_string(),
+            magician: "Dasher could blast you away with a snow magic power of 9001".to_string(),
+            consumer: "Dancer ate lots of candies, but also some grass".to_string(),
+        };
+
+        let result = candy(axum::Json(deers)).await;
+
+        assert_eq!(result.1.fastest, res.fastest);
+        assert_eq!(result.1.tallest, res.tallest);
+        assert_eq!(result.1.magician, res.magician);
+        assert_eq!(result.1.consumer, res.consumer);
+    }
+
+    #[tokio::test]
+    async fn test_deer_strength() {
+        let deer = vec![
+            DeerStrength {
+                name: "Dasher".to_string(),
+                strength: 5,
+            },
+            DeerStrength {
+                name: "Dancer".to_string(),
+                strength: 6,
+            },
+            DeerStrength {
+                name: "Prancer".to_string(),
+                strength: 4,
+            },
+            DeerStrength {
+                name: "Vixen".to_string(),
+                strength: 7,
+            },
+        ];
+        let result = deer_strength(axum::Json(deer)).await;
+
+        assert_eq!(result.1, "22");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 use days::{
     day0::{error_status, hello_world},
     day1::cube_the_bits,
+    day4::{candy, deer_strength},
 };
 
 mod days;
@@ -10,7 +14,9 @@ async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
         .route("/-1/error", get(error_status))
-        .route("/1/*param", get(cube_the_bits));
+        .route("/1/*param", get(cube_the_bits))
+        .route("/4/strength", post(deer_strength))
+        .route("/4/contest", post(candy));
 
     Ok(router.into())
 }
